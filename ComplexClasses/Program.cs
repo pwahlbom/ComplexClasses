0f94bce4654543a68a1f97990671ffc5d2464f98
Philip Wahlbom <philip@wahlbom.net>
2022-02-05 17:39:35 -0500

    Update Program.cs

--- ComplexClasses/Program.cs
+++ ComplexClasses/Program.cs
@@ -6,10 +6,61 @@ using System.Threading.Tasks;
 
 namespace ComplexClasses
 {
-    class Program
+        public class Address {
+            public string City { get; set; }
+            public string Street { get; set; }
+            public int Zip { get; set; }
+
+        public Address(string city, string street, int zip) {
+            City = city;
+            Street = street;
+            Zip = zip;
+        }
+    }
+
+    class Customer
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        // Billing Address
+        public Address BillingAddress { get; set; }
+        //public string BillingAddressStreet { get; set; }
+        //public string BillingAddreessCity { get; set; }
+        //public string BillingAddressZIP { get; set; }
+
+        // Shipping Address
+        public Address ShippingAddress { get; set; }
+        //public string ShippingAddressStreet { get; set; }
+        //public string ShippingAddressCity { get; set; }
+        //public string ShippingAddressZIP { get; set; }
+
+        public Customer(string firstName, string lastName, int age, string bCity, string bStreet, int bZip, string sCity, string sStreet, int sZip) {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            BillingAddress = new Address(bCity, bStreet, bZip);
+            ShippingAddress = new ComplexClasses.Address(sCity, sStreet, sZip);
+        }
+        
+    }
+        class Program
     {
         static void Main(string[] args)
         {
+            //method 1: var cust = new Customer("Andrew", "Buttram", 21);
+            //method 2: cust.BillingAddress = new Customer.Address ("Santa Maria", "Carolyne Way", 12345);
+            //method 2: cust.ShippingAddress = new Address("Santa Philipe", "Carolyne Way", 12345);
+            var cust = new Customer("Phil", "Wahlbom", 48, "Ball Ground", "Hightower Lake Trail", 12345, "Canton", "Riverstone Pkwy", 12345);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat(cust.LastName);
+            sb.AppendFormat(cust.BillingAddress.City);
+            sb.AppendFormat(cust.ShippingAddress.City);
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
         }
     }
 }
